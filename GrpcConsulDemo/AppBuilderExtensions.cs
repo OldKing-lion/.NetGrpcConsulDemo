2709615e846c6ee208e04765e71dc5c004b56b2e
lionkon <342501033@qq.com>
2022-03-30 15:30:51 +0800

    新增一个服务，用Consul官方组件注册

---
+++ GrpcConsulDemo/AppBuilderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Consul;
+
+namespace GrpcConsulDemo
+{
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder RegisterConsul(this IApplicationBuilder app, IConfiguration configuration, IHostApplicationLifetime lifetime)
+        {
+            var options = configuration.Get<ConsulServiceOption>();
+            var consul = new ConsulClient(x =>
+            {
+                x.Address = new Uri(options.ConsulUrl);
+            });
+
+
+            var registration = new AgentServiceRegistration
+            {
+                ID = Guid.NewGuid().ToString(),//服务实例唯一标识
+                Name = options.ServiceName,//服务名称
+                Address = options.ServiceHost,//服务IP地址
+                Port = options.ServicePort,
+                Check = new AgentServiceCheck//健康检查
+                {
+                    DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),//有人说这个是标识服务启动多久后注册，但是字面意思好像是取消注册,应该是健康检查多久没通过取消注册
+                    Interval = TimeSpan.FromSeconds(5),//间隔时间
+                    Timeout = TimeSpan.FromSeconds(5),//超时时间
+                    GRPC = options.HealthCheckHost,
+                    GRPCUseTLS = options.Usetls
+                }
+            };
+
+            consul.Agent.ServiceRegister(registration);//服务注册
+            //程序终止取消注册
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                consul.Agent.ServiceDeregister(registration.ID).Wait();
+            });
+            return app;
+        }
+    }
+}
---
+++ GrpcConsulDemo/ConsulServiceOption.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace GrpcConsulDemo
+{
+    public class ConsulServiceOption:IOptions<ConsulServiceOption>
+    {
+        public ConsulServiceOption()
+        {
+        }
+        public string ConsulUrl { get; set; } = "127.0.0.1:8500";
+        public string HealthCheckHost { get; set; } = "http://localhost:5192";
+        public bool Usetls { get; set; } = false;
+        public int TimeOutSeconds { get; set; } = 10;
+        public int IntevalSeconds { get; set; } = 10;
+        public string ServiceName { get; set; } = string.Empty;
+        public string ServiceHost { get; set; } = string.Empty;
+        public int ServicePort { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+
+        ConsulServiceOption IOptions<ConsulServiceOption>.Value => this;
+    }
+}
